146257b00230d9190929423b58797d30f220e40d
joaquingalang <galangjoaquin@gmail.com>
2025-09-26 14:16:11 +0800

    Add: contact section

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,4 +1,5 @@
 import { ReactComponent as StarIcon } from "../assets/star_icon.svg";
+import GroupPhoto from "../assets/group_photo.png";
 import NavBar from "../components/NavBar";
 import ResearcherTile from "../components/ResearcherTile";
 import FeatureCard from "../components/FeatureCard";
@@ -9,6 +10,7 @@ function HomePage() {
     <div>
       <NavBar />
 
+      {/* Hero Section */}
       <section name="hero" className="relative h-[40rem] overflow-hidden">
         <Waves/>
         <div className="relative z-10 h-full mx-[97px] grid grid-cols-2">
@@ -57,7 +59,7 @@ function HomePage() {
       </section>
 
       {/* Features Section */}
-      <section name="hero"  className="relative h-[40rem] overflow-hidden">
+      <section name="features"  className="relative h-[40rem] overflow-hidden">
         <Waves/>
         <div className="relative z-10 h-[40rem] mx-[97px] flex flex-col justify-center items-center">
           <div className="flex mb-[3.5rem]">
@@ -117,6 +119,40 @@ function HomePage() {
           </div>
         </div>
       </section>
+
+      {/* Contact Section */}
+      <section name="contact"  className="relative overflow-hidden">
+        <Waves/>
+        <div className="relative z-10 mx-[97px] my-[80px]">
+          <div className="w-full h-full grid grid-cols-2 gap-40">
+              <div className="w-full h-full bg-brand-indigo-400 rounded-xl p-10">
+                <form action="">
+                  <p className="text-white text-xl font-outfit font-light">GET IN TOUCH</p>
+
+                  <p className="text-white text-[56px] font-outfit font-extrabold">Contact.</p>
+
+                  <label htmlFor="name" className="text-white text-xl font-outfit font-medium">Your Name</label>
+                  <input id="name" placeholder="What's your name?" className="w-full bg-brand-indigo-300 rounded-xl mt-2 mb-5 px-5 py-3"/>
+
+                  <label htmlFor="email" className="text-white text-xl font-outfit font-medium">Your Email</label>
+                  <input id="email" placeholder="What's your email?" className="w-full bg-brand-indigo-300 rounded-xl mt-2 mb-5 px-5 py-3"/>
+                  
+                  <p htmlFor="message" className="text-white text-xl font-outfit font-medium">Your Message</p>
+                  <textarea id="message" placeholder="What do you want to say?" className="w-full h-[15rem] bg-brand-indigo-300 rounded-xl mt-2 mb-5 px-5 py-3"></textarea>
+
+                  <div className="flex justify-end">
+                    <input type="submit" value="Send Message" className="rounded-lg text-white text-lg bg-brand-turqoise-200 font-semibold px-5 py-2 cursor-pointer"/>
+                  </div>
+                </form>
+              </div>
+
+              <div className="w-full h-full flex justify-center items-center">
+                <img src={GroupPhoto} alt="Group 1" className="relative z-0" />
+              </div>
+          </div>
+        </div>
+      </section>
+
     </div>
   );
 }
