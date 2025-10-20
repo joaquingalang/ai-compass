59386598aa90a5e50981bb3a9d338169295776d0
joaquingalang <galangjoaquin@gmail.com>
2025-10-21 00:46:34 +0800

    Update: routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import AnalysisPage from "./pages/AnalysisPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import AnalysisPage from "./pages/AnalysisPage";
 
 function App() {
   return (
-    <AnalysisPage></AnalysisPage>
-    // <HomePage></HomePage>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage/>}/>
+          <Route path="/analysis" element={<AnalysisPage/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+    
   );
 }
 
